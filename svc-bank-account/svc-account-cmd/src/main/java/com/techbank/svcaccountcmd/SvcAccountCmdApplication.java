e52e755252b29a33df094f9314e86e3a8cccf9c0
Faizal Ali Mir <faizalali.mir@citizensbank.com>
2023-03-10 16:21:18 -0500

    Added

---
+++ svc-bank-account/svc-account-cmd/src/main/java/com/techbank/svcaccountcmd/SvcAccountCmdApplication.java
@@ -0,0 +1,13 @@
+package com.techbank.svcaccountcmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvcAccountCmdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SvcAccountCmdApplication.class, args);
+    }
+
+}
---
+++ svc-bank-account/svc-account-query/src/test/java/com/techbank/svcaccountquery/SvcAccountQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techbank.svcaccountquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvcAccountQueryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
